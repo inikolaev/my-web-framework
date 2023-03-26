bcd371ebc10c56bfc822a3eccd120c4f41f2f143
Igor Nikolaev <igor.nikolaev@nikisoft.ru>
2023-03-26 22:12:37 +0300

    Add example of a custom annotation

--- my_web_framework/main.py
+++ my_web_framework/main.py
@@ -15,11 +15,16 @@ def get_ip_address(request: Request, id: str) -> str:
     return f"{request.client.host}::{id}"
 
 
+def limit_by_ip_address(expression: str):
+    """We can define custome annotations for easier reuse"""
+    return limit(expression, key=get_ip_address)
+
+
 class Controller(BaseController):
     @get("/payments/{id}")
     @limit("1/minute", key=get_ip_address)
     @limit("10/hour", key=get_ip_address)
-    @limit("100/day", key=get_ip_address)
+    @limit_by_ip_address("100/day")
     @awesome()
     async def get_payment(self, request: Request, id: str):
         logger.info("Hello world")
